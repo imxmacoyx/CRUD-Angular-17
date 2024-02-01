8b510108aa99da215f5d9da98c9cc55705a10155
Marco Valle <marco_97@live.com.mx>
2024-02-01 13:41:58 -0600

    feat: Estructura principal del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { TablaComponent } from './components/tabla/tabla.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuComponent, TablaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TablaComponent } from './components/tabla/tabla.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'tabla',
+        pathMatch: 'full'
+    },
+    {
+        path:'tabla',
+        component: TablaComponent
+    }
+];
